455bb6a25f91f6b2688e74d695a26409ea845ccc
chrislo27 <kinchrislo@gmail.com>
2020-06-04 19:28:13 -0700

    Add hair space and neg-ack support

--- src/main/kotlin/io/github/chrislo27/dotmatrix/Layout.kt
+++ src/main/kotlin/io/github/chrislo27/dotmatrix/Layout.kt
@@ -7,21 +7,33 @@ import kotlin.math.roundToInt
 
 
 class GlyphRun(val font: DotMtxFont, val text: String) {
-    
+
     val missingChars: Set<Char> = mutableSetOf()
     val glyphPositions: List<GlyphPosition>
     val width: Int
     val height: Int = font.height
     val lastAdvance: Int
-    
+
     init {
         missingChars as MutableSet
         val height = font.height
         var currentX = 0
         var lastAdv = 0
+        var hairSpaces = 0
         glyphPositions = text.mapNotNull { c ->
             val glyph = font.glyphs[c]
-            if (glyph == null) {
+            if (c == '\u200A') {
+                // Hair space -- advances by one pixel only
+                currentX++
+                lastAdv = 1
+                hairSpaces++
+                null
+            } else if (c == '\u0015') {
+                // Negative-ack -- advances by negative one pixel only
+                currentX--
+                lastAdv--
+                null
+            } else if (glyph == null) {
                 missingChars.add(c)
                 null
             } else {
@@ -31,10 +43,10 @@ class GlyphRun(val font: DotMtxFont, val text: String) {
                 }
             }
         }
-        width = ((glyphPositions.map { it.x + it.glyph.w }.max() ?: 0) - (glyphPositions.map { it.x }.min() ?: 0)).absoluteValue
+        width = (((glyphPositions.map { it.x + it.glyph.w }.max() ?: 0) - (glyphPositions.map { it.x }.min() ?: 0)).absoluteValue + hairSpaces).coerceAtLeast(0)
         lastAdvance = lastAdv
     }
-    
+
     fun toBufferedImage(): BufferedImage {
         val subimageCache: MutableMap<Glyph, BufferedImage> = mutableMapOf()
         return BufferedImage(width.coerceAtLeast(1), height, BufferedImage.TYPE_4BYTE_ABGR).apply {
@@ -49,21 +61,21 @@ class GlyphRun(val font: DotMtxFont, val text: String) {
             g.dispose()
         }
     }
-    
+
     data class GlyphPosition(val glyph: Glyph, val x: Int, val y: Int)
-    
+
 }
 
 data class GlyphLayout(val runs: List<GlyphRun>, val glyphAlign: List<VerticalAlignment>, val horizontalAlign: TextAlignment) {
-    
+
     constructor(runs: List<GlyphRun>, glyphAlign: VerticalAlignment = VerticalAlignment.BOTTOM, horizontalAlign: TextAlignment = TextAlignment.CENTRE)
             : this(runs, if (runs.isEmpty()) emptyList() else List(runs.size) { glyphAlign }, horizontalAlign)
-    
+
     val width: Int = if (runs.isEmpty()) 0 else (runs.sumBy { r ->
         r.width + r.lastAdvance
     } - runs.last().lastAdvance)
     val height: Int = runs.maxBy { it.height }?.height ?: 0
-    
+
     fun toBufferedImage(): BufferedImage {
         if (width == 0 || height == 0) return BufferedImage(1, 1, BufferedImage.TYPE_4BYTE_ABGR)
         return BufferedImage(width, height, BufferedImage.TYPE_4BYTE_ABGR).apply {
@@ -85,10 +97,10 @@ data class GlyphLayout(val runs: List<GlyphRun>, val glyphAlign: List<VerticalAl
 }
 
 data class LayoutLines(val lines: List<GlyphLayout>, val lineSpacing: LineSpacing = LineSpacing.FLUSH_TO_EDGES) {
-    
+
     val totalWidth: Int = lines.maxBy { it.width }?.width ?: 0
     val totalHeight: Int = lines.sumBy { it.height }
-    
+
     fun toBufferedImage(width: Int, height: Int): BufferedImage {
         if (width == 0 || height == 0) error("Width and height must each be greater than zero")
         return BufferedImage(width, height, BufferedImage.TYPE_4BYTE_ABGR).apply {
@@ -103,14 +115,14 @@ data class LayoutLines(val lines: List<GlyphLayout>, val lineSpacing: LineSpacin
             if (lines.size == 1) {
                 val layout = lines.first()
                 val img = layout.toBufferedImage()
-                g.drawImage(img, layout.determineX(), ((height - layout.height) / 2f).toInt(), null)
+                g.drawImage(img, layout.determineX(), ((height - layout.height) / 2f).roundToInt(), null)
             } else {
                 when (lineSpacing) {
                     LineSpacing.FLUSH_TO_EDGES -> {
                         val totalHeight = totalHeight.coerceAtLeast(1)
                         val spacing: Float = (height - totalHeight).toFloat() / (lines.size - 1).coerceAtLeast(1)
                         lines.fold(0f) { acc, layout ->
-                            val y = acc.toInt()
+                            val y = acc.roundToInt()
                             g.drawImage(layout.toBufferedImage(), layout.determineX(), y, null)
                             acc + layout.height + spacing
                         }
@@ -119,7 +131,7 @@ data class LayoutLines(val lines: List<GlyphLayout>, val lineSpacing: LineSpacin
                         val totalHeight = totalHeight.coerceAtLeast(1)
                         val spacing: Float = (height - totalHeight).toFloat() / (lines.size + 1).coerceAtLeast(1)
                         lines.fold(spacing) { acc, layout ->
-                            val y = acc.toInt()
+                            val y = acc.roundToInt()
                             g.drawImage(layout.toBufferedImage(), layout.determineX(), y, null)
                             acc + layout.height + spacing
                         }
@@ -129,5 +141,5 @@ data class LayoutLines(val lines: List<GlyphLayout>, val lineSpacing: LineSpacin
             g.dispose()
         }
     }
-    
+
 }
