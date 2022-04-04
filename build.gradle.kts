927450ba7da35790c0a26ef05ce7d6f603a12734
chrislo27 <kinchrislo@gmail.com>
2022-04-03 17:45:06 -0700

    Update to Kotlin 1.6, bump version to 3.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,9 @@
 plugins {
-    kotlin("jvm") version "1.5.10"
+    kotlin("jvm") version "1.6.10"
     `maven-publish`
 }
 group = "io.github.chrislo27"
-version = "3.0-SNAPSHOT"
+version = "3.1"
 
 repositories {
     mavenCentral()
@@ -12,7 +12,7 @@ kotlin {
     sourceSets {
         val main by getting {
             dependencies {
-                implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.0.1")
+                implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.2")
                 implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
                 implementation("com.madgag:animated-gif-lib:1.4")
             }
